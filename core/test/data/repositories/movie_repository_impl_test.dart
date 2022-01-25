aee2e1f423121d73aecb887a4804bb09a2a2ae6a
gabriella <gabriellafranchesca22@gmail.com>
2022-01-25 13:56:59 +0700

    test codemagic testing

--- core/test/data/repositories/movie_repository_impl_test.dart
+++ core/test/data/repositories/movie_repository_impl_test.dart
@@ -11,7 +11,7 @@ import 'package:core/domain/entities/movie.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../dummy_data/dummy_objects.dart';
+import '../../../../test/dummy/dummy_objects.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
