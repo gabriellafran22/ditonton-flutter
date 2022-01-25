fb352b441a56638100c58a14ec362f1df3a3d0db
gabriella <gabriellafranchesca22@gmail.com>
2022-01-25 13:44:18 +0700

    test codemagic testing (last time error when test files moved to core)

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -50,7 +50,7 @@ export 'presentation/pages/movie_detail_page.dart';
 export 'presentation/pages/home_movie_page.dart';
 export 'presentation/pages/popular_movies_page.dart';
 export 'presentation/pages/top_rated_movies_page.dart';
-export 'presentation/pages/watchlist_movies_page.dart';
+export 'presentation/pages/watchlist_page.dart';
 // presentation -> widgets
 export 'presentation/widgets/movie_card_list.dart';
 // presentation -> cubit
