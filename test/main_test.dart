9080c30b0babbc41bc2310e6adbf16d96b8734d9
gabriella <gabriellafranchesca22@gmail.com>
2022-01-26 16:02:40 +0700

    add testing for codemagic

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,1 +1,42 @@
-void main () {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movies/movies.dart';
+
+// bagian ini hanya untuk codemagic spy tidak error
+void main () {
+  const tMovieModel = MovieModel(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  test('should be a subclass of Movie entity', () async {
+    final result = tMovieModel.toEntity();
+    expect(result, tMovie);
+  });
+}
